54865dfc2fd0aef09dd02e74a8fbf70837af1966
deepakpixel <gtx.addicted@gmail.com>
2020-03-25 21:56:33 +0530

    fixed overflow

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include "header.html";
 // include "body.php";
 ?>
-<img src="./assets/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" style="margin:0px;padding:0px;display:block">
+<img src="./assets/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" height="100%"
+    style="margin:0px;padding:0px;display:block">
 <?php include "footer.html";
 ?>
