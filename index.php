0ab4da66fdae4117844aecff369faf8f128a3f79
deepakpixel <gtx.addicted@gmail.com>
2020-03-28 16:24:38 +0530

    image url changed to /var/www/html/microbird-website

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-include "header.html";
+include "./resources/config/header.html";
 // include "body.php";
 ?>
-<img src="./assets/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" height="100%"
+<img src="./resources/images/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" height="100%"
     style="margin:0px;padding:0px;display:block">
-<?php include "footer.html";
+<?php include "./resources/config/footer.html";
 ?>
