ef6c7a6094a32f69ab6f8828152ca033c562a9bf
deepakpixel <gtx.addicted@gmail.com>
2020-03-29 21:12:34 +0530

    website under maintainance

--- index.php
+++ index.php
@@ -1,8 +1,67 @@
+<title>Microbird YMCA</title>
 <?php
 include "./resources/config/header.html";
 // include "body.php";
 ?>
-<img src="./resources/images/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" height="100%"
-    style="margin:0px;padding:0px;display:block">
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <center>
+        <h1>Sorry for inconvenience</h1>
+        <center> <strong>Website under maintainance</strong> </center>
+        <br>
+
+    </center>
+    <!-- 
+    <div class="container">
+        <img src="./resources/images/robot-wallpaper-18.jpg" alt="Wallpaper" width="100%" height="100%"
+            style="margin:0px;padding:0px;display:block">
+        <div class="text">
+            <div class="heading">
+                Microbird
+            </div>
+
+            <div class="tagline">
+                Techno Club
+            </div>
+        </div>
+
+    </div> -->
+
+
+</body>
+
+</html>
 <?php include "./resources/config/footer.html";
 ?>
+
+
+
+
+<style>
+.text {
+    font-size: 100px;
+    position: fixed;
+    padding-top: 100px;
+}
+
+.container {
+    color: white;
+    font-family: Helvetica, sans-serif;
+
+    /* background: url(/resources/images/robot-wallpaper-18.jpg) center no-repeat fixed; */
+    /* background-size: 100%; */
+    height: 100vh;
+    width: 100%;
+}
+
+img {
+    float: left;
+}
+</style>
