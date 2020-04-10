f7ee2c217cb610cd2bfd2349448a0cb08f506dbb
deepakpixel <gtx.addicted@gmail.com>
2020-04-10 14:50:24 +0530

    max imit

--- events/techchef/login/round2/upload.php
+++ events/techchef/login/round2/upload.php
@@ -1,4 +1,8 @@
 <?php 
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '20M');
+ini_set('max_input_time', 300);
+ini_set('max_execution_time', 300);
 $upload = 'err'; 
 if(!empty($_FILES['file'])){ 
     
